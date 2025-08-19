d6af834602a7c4ee18940f95b6eff676e3be9389
Akshat Kishore <akshatkishore@berkeley.edu>
2025-08-19 12:40:27 -0700

    added API integration - error in finding server.

---
+++ Goplai/ClipsView.swift
@@ -0,0 +1,18 @@
+//
+//  ClipsView.swift
+//  Goplai
+//
+//  Created by Akshat Kishore on 8/8/25.
+//
+
+import SwiftUI
+
+struct ClipsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClipsView()
+}
---
+++ Goplai/VideoPicket.swift
@@ -0,0 +1,18 @@
+//
+//  VideoPicket.swift
+//  Goplai
+//
+//  Created by Akshat Kishore on 8/8/25.
+//
+
+import SwiftUI
+
+struct VideoPicket: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideoPicket()
+}
